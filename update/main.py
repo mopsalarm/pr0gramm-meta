433b05b3b5184da650e03c110be922b79e1e8353
Mopsalarm <mopsalarm@users.noreply.github.com>
2015-06-04 20:21:45 +0200

    Bugfix

--- update/main.py
+++ update/main.py
@@ -270,13 +270,18 @@ def main():
     create_database_tables(db)
 
     def start():
-        yield schedule(60, "pr0gramm.meta.update.sizes", run, db, (0, 0.5, update_item_sizes), (0, 0.5, update_item_infos))
-        yield schedule(600, "pr0gramm.meta.update.infos.new", run, db, (0, 6, update_item_infos))
-        yield schedule(3600, "pr0gramm.meta.update.infos.more", run, db, (5, 48, update_item_infos))
+        yield schedule(60, "pr0gramm.meta.update.sizes",
+                       run, db, (0, 0.5, update_item_sizes), (0, 0.5, update_item_infos))
+
+        yield schedule(600, "pr0gramm.meta.update.infos.new",
+                       run, db, (0, 6, update_item_infos))
+
+        yield schedule(3600, "pr0gramm.meta.update.infos.more",
+                       run, db, (5, 48, update_item_infos))
 
         yield gevent.spawn(run, db, (48, 24 * 7, update_item_infos))
 
-    gevent.joinall(start())
+    gevent.joinall(tuple(start()))
 
 if __name__ == '__main__':
     file_handler = logbook.FileHandler("logfile.log", bubble=True)
