c6ca1a4aa0a104635e2011ac3cb17477f4112c5f
Mopsalarm <mopsalarm@users.noreply.github.com>
2015-08-29 12:45:08 +0200

    Remove creation of uneccessary index

--- update/main.py
+++ update/main.py
@@ -315,7 +315,6 @@ def create_database_tables(db):
     )""")
 
     db.execute("CREATE INDEX IF NOT EXISTS tags_item_id ON tags(item_id)")
-    db.execute("CREATE INDEX IF NOT EXISTS tags_tag ON tags(tag COLLATE NOCASE)")
     db.execute("CREATE INDEX IF NOT EXISTS users_name ON users(name COLLATE NOCASE)")
     db.execute("CREATE INDEX IF NOT EXISTS user_score__user_id__timestamp ON user_score(user_id, timestamp)")
 
