14263d424d76f96137cbea5e0440af41b00486e0
Mopsalarm <mopsalarm@users.noreply.github.com>
2015-08-29 11:24:39 +0200

    Simplified repost query

--- webapp/service.py
+++ webapp/service.py
@@ -19,7 +19,8 @@ def metric_name(suffix):
     return "pr0gramm.meta.webapp.%s" % suffix
 
 
-def query_sizes(where_clause):
+def query_sizes(item_ids):
+    where_clause = "items.id IN (%s)" % ",".join(str(val) for val in item_ids)
     query = "SELECT items.id, width, height FROM items" \
             " JOIN sizes ON items.id=sizes.id " \
             " WHERE %s" \
@@ -31,10 +32,10 @@ def query_sizes(where_clause):
     ]
 
 
-def query_reposts(where_clause):
-    query = "SELECT DISTINCT items.id FROM items " \
-            " JOIN tags ON items.id=tags.item_id " \
-            " WHERE %s AND tags.confidence>0.3 AND tags.tag='repost' COLLATE NOCASE" \
+def query_reposts(item_ids):
+    where_clause = "item_id IN (%s)" % ",".join(str(val) for val in item_ids)
+    query = "SELECT item_id FROM tags " \
+            " WHERE %s AND confidence>0.3 AND tag='repost' COLLATE nocase" \
             " LIMIT 150" % where_clause
 
     return [item_id for item_id, in database.execute(query).fetchall()]
@@ -47,11 +48,9 @@ def items():
     start_time = time.time()
     item_ids = [int(val) for val in bottle.request.params.get("ids", []).split(",") if val][:150]
 
-    where_clause = "items.id IN (%s)" % ",".join(str(val) for val in item_ids)
-
     result = attrdict()
-    result.sizes = query_sizes(where_clause)
-    result.reposts = query_reposts(where_clause)
+    result.sizes = query_sizes(item_ids)
+    result.reposts = query_reposts(item_ids)
     result.duration = time.time() - start_time
     return result
 
@@ -61,7 +60,7 @@ def items():
 def user_benis(user):
     query = "SELECT user_score.timestamp, user_score.score" \
             " FROM user_score, users" \
-            " WHERE users.name=? COLLATE NOCASE AND users.id=user_score.user_id AND user_score.timestamp>?"
+            " WHERE users.name=? COLLATE nocase AND users.id=user_score.user_id AND user_score.timestamp>?"
 
     start_time = int(time.time() - 3600 * 24 * 7)
     return {"benisHistory": database.execute(query, [user, start_time]).fetchall()}
